d7154df7adb478177c4c930a6644ace4936e0a76
WLeondike <billyleondike@gmail.com>
2020-07-21 16:09:43 -0400

    fixed small typo

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name, devour) 
+INSERT INTO burgers (name, devoured) 
 VALUES ('Beyond', 1), ('Bacon', 1), ('Boca', 0),
        ('Black Bean', 0), ('Impossible', 0), ('Patty Melt', 1), 
        ('Veggie', 0);
